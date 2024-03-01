02135d508f65176f4429d4206a18dc0d762c67b0
Kibet <mshckkibet@gmail.com>
2024-03-01 18:01:08 +0300

    added channelDetails functionality

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,7 +3,7 @@ import { Box, CardContent, CardMedia, Typography} from '@mui/material'
 import { CheckCircle } from '@mui/icons-material'
 import { Link } from 'react-router-dom'
 import { demoProfilePicture } from '../utils/constants'
-function ChannelCard({channelDetail}) {
+function ChannelCard({channelDetail, marginTop}) {
   return (
     <Box 
         sx={{
@@ -13,8 +13,9 @@ function ChannelCard({channelDetail}) {
             justifyContent:'center',
             alignItems:'center',
             width:{xs:'356px', md:'320px'},
-            height:'326px',
-            margin: 'auto'
+            height:'326px', 
+            margin: 'auto',
+            marginTop:marginTop
         }}>
         <Link to ={`/channel/${channelDetail?.id?.channelId}`}>
             <CardContent sx={{display:'flex', flexDirection:'column', justifyContent:'center', textAlign:'center', color:'#fff'}}>
